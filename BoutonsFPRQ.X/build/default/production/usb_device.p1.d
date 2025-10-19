6d061c0a25c58810cd49d3421695422fefba34d9
ilias <105916678+Ilias95000@users.noreply.github.com>
2025-10-19 18:28:06 +0200

    Salam c les boutons qui envoie FLAP/PAUSE/REPRENDRE/QUITTER

---
+++ BoutonsFPRQ.X/build/default/production/usb_device.p1.d
@@ -0,0 +1,12 @@
+build/default/production/usb_device.p1:  \
+usb_device.c  \
+usb_device_config.h  \
+usb.h  \
+usb_common.h  \
+usb_ch9.h  \
+usb_device.h  \
+usb_hal.h  \
+usb_hal_pic18.h  \
+fixed_address_memory.h  \
+usb_device_cdc.h  \
+usb_device_local.h 
---
+++ BoutonsFPRQ.X/build/default/production/usb_device_cdc.p1.d
@@ -0,0 +1,11 @@
+build/default/production/usb_device_cdc.p1:  \
+usb_device_cdc.c  \
+usb.h  \
+usb_device_config.h  \
+usb_common.h  \
+usb_ch9.h  \
+usb_device.h  \
+usb_hal.h  \
+usb_hal_pic18.h  \
+fixed_address_memory.h  \
+usb_device_cdc.h 
